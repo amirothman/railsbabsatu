3e548728cce291f305ec34f956bf6cc9e17d62cc
amir <th3go2@gmail.com>
2013-12-01 17:40:23 +0100

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babsatu::Application.config.secret_key_base = '1d7d959b25ff3f3d7c046f44ae8a363d390e28b82d3e500f6bbc7d8e0f255582487ce13cbdfde735bf72d62655e25d2cd02b3343330dfa3746841ff80be4f313'
